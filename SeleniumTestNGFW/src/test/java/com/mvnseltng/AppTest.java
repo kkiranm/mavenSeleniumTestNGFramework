ddb2347901f1f073d57809877d83c5ba644985b9
Kiran Mudigonda <kiran.mudigonda@outlook.com>
2017-09-14 14:03:43 +1000

    Updated pom.xml, AppTest.java

--- SeleniumTestNGFW/src/test/java/com/mvnseltng/AppTest.java
+++ SeleniumTestNGFW/src/test/java/com/mvnseltng/AppTest.java
@@ -1,38 +1,16 @@
 package com.mvnseltng;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void smoke(){
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://www.aami.com.au");
+				
+	}
 }
